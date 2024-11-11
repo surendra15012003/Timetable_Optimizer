04a6b61d601ceb4682bb78d6a15240228f1c1301
surendra15012003 <144783566+surendra15012003@users.noreply.github.com>
2024-11-11 23:23:56 +0530

    Initial Commit

---
+++ build_deploy_netlify.sh
@@ -0,0 +1,1 @@
+npm run build; sleep 5; echo "build/" | netlify deploy --prod
---
+++ craco.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // plugins: [{ plugin: require('@semantic-ui-react/craco-less') }],
+    webpack: {
+        alias: {
+            /*
+            "react": "preact/compat",
+            "react-dom": "preact/compat",
+            */
+        }
+    }
+}
